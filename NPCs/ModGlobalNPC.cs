dfbfcafbf514ea45bb35c47bc2e88a43f6d6af3c
Jerry Wester <JerryWester@users.noreply.github.com>
2021-03-04 20:05:54 -0700

    Add project files.

---
+++ NPCs/ModGlobalNPC.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ReduceGuardianHealth.NPCs
+{
+    class ModGlobalNPC : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            if (npc.type == NPCID.DungeonGuardian)
+            {
+                npc.lifeMax = 999;
+            }
+        }
+    }
+}
---
+++ ReduceGuardianHealth.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace ReduceGuardianHealth
+{
+	public class ReduceGuardianHealth : Mod
+	{
+	}
+}
